cc202e94a9c9c68996d6bb58b1632ae0dd266104
[devanandd7] <crosseye315@gmail.com>
2026-01-16 12:48:02 +0530

    recheck and update the secert key and tokens

--- src/lib/mongo.js
+++ src/lib/mongo.js
@@ -1,8 +1,8 @@
 import { MongoClient } from "mongodb";
 import 'dotenv/config';
 
-const uri = process.env.MONGODB_URI||"";
-const dbName = process.env.MONGODB_DB || "appdb";
+const uri = process.env.MONGODB_URI;
+const dbName = process.env.MONGODB_DB ;
 
 if (!uri) {
   throw new Error("Missing MONGODB_URI in environment variables");
